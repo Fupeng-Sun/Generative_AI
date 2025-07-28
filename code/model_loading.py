1c03052db8b8cb0657c69984b6b3b5342436469d
Fupeng <fs423@ic.ac.uk>
2025-07-28 19:07:03 +0100

    also add the previous data preparation

--- code/model_loading.py
+++ code/model_loading.py
@@ -1,3 +1,16 @@
+# Prepare the Dataset
+from datasets import load_dataset
+
+# Load the dataset
+dataset = load_dataset("imdb")
+
+# Create label mappings
+labels = dataset["train"].features["label"].names # ["neg", "pos"]
+num_labels = len(labels)
+label2id = {label: i for i, label in enumerate(labels)}
+id2label = {i: label for i, label in enumerate(labels)}
+
+# Load Model and Tokenizer
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # Model ID from Hugging Face Hub
